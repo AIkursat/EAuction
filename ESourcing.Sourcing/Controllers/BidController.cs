77c831180be385b3ac506bc4d10693642d55fb51
AIKursat <aynurkursat1999@hotmail.com>
2022-10-18 16:58:48 +0300

    Create and iplement BidController

--- ESourcing.Sourcing/Controllers/BidController.cs
+++ ESourcing.Sourcing/Controllers/BidController.cs
@@ -1,11 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Esourcing.Sourcing.Repositories.Interfaces;
+using ESourcing.Sourcing.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
-namespace ESourcing.Sourcing.Controllers
+namespace Esourcing.Sourcing.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
     public class BidController : ControllerBase
     {
+        private readonly IBidRepository _bidRepository;
+
+        public BidController(IBidRepository bidRepository)
+        {
+            _bidRepository = bidRepository;
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult> SendBid([FromBody] Bid bid)
+        {
+            await _bidRepository.SendBid(bid);
+
+            return Ok();
+        }
+
+        [HttpGet("GetBidByAuctionId")]
+        [ProducesResponseType(typeof(IEnumerable<Bid>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetBidByAuctionId(string id)
+        {
+            IEnumerable<Bid> bids = await _bidRepository.GetBidsByAuctionId(id);
+
+            return Ok(bids);
+        }
+
+        [HttpGet("GetAllBidsByAuctionId")]
+        [ProducesResponseType(typeof(List<Bid>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Bid>>> GetAllBidsByAuctionId(string id)
+        {
+            IEnumerable<Bid> bids = await _bidRepository.GetAllBidsByAuctionId(id);
+
+            return Ok(bids);
+        }
+
+        [HttpGet("GetWinnerBid")]
+        [ProducesResponseType(typeof(Bid), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Bid>> GetWinnerBid(string id)
+        {
+            Bid bid = await _bidRepository.GetWinnerBid(id);
+
+            return Ok(bid);
+        }
+
     }
 }
